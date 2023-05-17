6327933ff1653a5f10168650664ba62765d795f9
Kate Fanina <katherina.fanina@gmail.com>
2023-05-17 21:04:48 +0300

    message eddit valiation

--- decorators/validateBody.js
+++ decorators/validateBody.js
@@ -4,7 +4,23 @@ const validateBody = schema => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      next(HttpError(400, error.message));
+      const { contactId } = req.params;
+      if (contactId) {
+        if (!Object.keys(req.body).length) {
+          next(HttpError(400, `missing fields`));
+        } else {
+          next(HttpError(400, error.message));
+        }
+      } else {
+        const requiredFields = ['name', 'email', 'phone'];
+        const missingFields = requiredFields.filter(
+          fieldName => !Object.keys(req.body).includes(fieldName)
+        );
+        if (missingFields.length > 0) {
+          const label = missingFields.join(', ');
+          next(HttpError(400, `missing required ${label} field(s)`));
+        }
+      }
     }
     next();
   };
